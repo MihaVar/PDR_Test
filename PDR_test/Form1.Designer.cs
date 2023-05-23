d9ed1977c43cdb02b70ac3152c809e74eb10ac7c
MihaVar <mykhailvarych@gmail.com>
2023-05-23 22:28:16 +0300

    added icom

--- PDR_test/Form1.Designer.cs
+++ PDR_test/Form1.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.lblQuestion = new System.Windows.Forms.Label();
             this.btnNext = new System.Windows.Forms.Button();
             this.timer = new System.Windows.Forms.TextBox();
@@ -233,6 +234,7 @@
             this.Controls.Add(this.btnExit);
             this.Controls.Add(this.btnStart);
             this.Controls.Add(this.pnlTest);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
             this.Text = "Тестування з ПДР";
             this.Load += new System.EventHandler(this.Form1_Load);
